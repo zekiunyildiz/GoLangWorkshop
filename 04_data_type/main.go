940da8553ee1d39ddc560504d922c8656ee212c6
zekiunyildiz <zekiunyildiz@gmail.com>
2021-09-14 14:31:35 +0300

    uygulama örnekleri

--- 04_data_type/main.go
+++ 04_data_type/main.go
@@ -4,10 +4,19 @@ import "fmt"
 
 func main() {
 
-	x := 10
+	/* x := 10
 	y := 10.0
 
 	fmt.Printf("%v %T\n", x, x)
 	fmt.Printf("%v %T\n", y, y)
+
+	fmt.Println(x + int(y)) */
+
+	var x int8 = 127
+	var y int16
+
+	y = int16(x)
+
+	fmt.Println(y)
 
 }
---
+++ 05_exApp/main.go
@@ -0,0 +1,45 @@
+/* package main
+
+import "fmt"
+
+func main() {
+
+	x := 75
+	var y float64
+	y = float64(x)
+	fmt.Println(y)
+}
+*/
+/*
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 5
+	y := 10
+
+	fmt.Println("X:", x, "Y", y)
+
+	x, y = y, x
+
+	fmt.Println("X:", x, "Y:", y)
+}
+*/
+
+/* package main
+
+import "fmt"
+
+func main() {
+	x := 5
+
+	if true {
+		x := 10
+		x++
+		fmt.Println(x)
+	}
+	fmt.Println(x)
+}
+*/
